90b77fff3fc2a2f84c601b15b2df271e14e44d0d
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-12-17 02:03:46 +0200

    add__imageGallery

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from 'style';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
